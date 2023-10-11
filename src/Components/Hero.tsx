45f30073a5ce44870259be8c0c1869b2bfd1ffea
Husam Mohamed <husamug@gmail.com>
2023-10-11 06:39:33 +0200

    increased button border radius. changed hero button to anchor tag

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <div className='container d-md-flex align-items-center py-5 hero'>
       <div className='w-100 text-center text-md-start py-5'>
         <h1 className='hero-title py-2'>Hello, I'm <span className='husam'>Husam</span> <br /> A Software Engineer</h1>
-        <button className='hero-btn btn btn-outline-primary'>ABOUT ME</button>
+        <a href='#about' className='hero-btn btn btn-outline-primary'>ABOUT ME</a>
       </div>
       <div className='w-100'>
         <img className='img-fluid' src={heroImg} alt="hero-image" />
